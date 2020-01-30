03b1a925f08926199fcee3014d9eac8553d1e3c7
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-29 18:30:43 -0700

    server setup - knexfile, db config, and index with welcome and error routes

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 4000;
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res, next) => {
+  res.json({
+    message: 'Welcome to our API',
+  });
+});
+
+server.use((req, res) => {
+  res.status(404).json({
+    message: 'Route was not found',
+  });
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: 'An internal error occurred, please try again later',
+  });
+});
+
+if (!module.parent) {
+  server.listen(port, host, () => {
+    console.log(`\n*** Server Running on http://${host}:${port} ***\n`);
+  });
+}
+
+module.exports = server;
